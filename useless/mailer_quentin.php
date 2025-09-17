15a10ba388c4966b1fd21b69ad436f600ae5f2f5
hadrien17200 <hadrien.chochon@student.junia.com>
2025-09-17 10:43:01 +0200

    Modif code php

--- useless/mailer_quentin.php
+++ useless/mailer_quentin.php
@@ -9,7 +9,7 @@ $message = "Salut fdp \r\n".
 			"Cordialement,\r\n".
 			"L'équipe Bet'eirb";
 $headers = "From: beteirb@enseirb-matmeca.fr" . "\r\n" .
-			//"Reply-To: qdeclercq@enseirb-matmeca.fr" .
+			"Reply-To: qdeclercq@enseirb-matmeca.fr" .
 			"X-Mailer: PHP/" . phpversion();
 
 mail($to, $subject, $message, $headers);
